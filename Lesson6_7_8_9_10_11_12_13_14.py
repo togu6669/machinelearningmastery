5b1c5f2a7ca0fa8d3978770ada9afcf41ab54b18
unknown <tguz@msol-n-010.msol>
2019-04-20 08:36:05 +0200

    Day 11 Lesson 11

--- Lesson6_7_8_9_10_11_12_13_14.py
+++ Lesson6_7_8_9_10_11_12_13_14.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import LeaveOneOut
 from sklearn.model_selection import cross_val_score
 from keras.callbacks import ModelCheckpoint
 from keras.layers import Dropout
+from keras.optimizers import SGD
 
 
 # Function to create model, required for KerasClassifier
@@ -22,6 +23,10 @@ model = KerasClassifier(build_fn=create_model, epochs=150, batch_size=10)
 
 # Lesson 9:for example, you can create a dropout layer with the probability of 20% and add it to your model as follows:
 model.add(Dropout(0.2))
+
+# lesson 10 Stochastic Gradient Descent
+sgd = SGD(lr=0.1, momentum=0.9, decay=0.0001, nesterov=False)
+model.compile(..., optimizer=sgd)
 
 # evaluate model using 10-fold cross validation in scikit-learn
 kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
